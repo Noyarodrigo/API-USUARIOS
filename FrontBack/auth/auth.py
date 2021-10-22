fd52e9eb4dc2d7542179dd1b3e7527efe2d1acc5
noyarodrigo <rodrinoya.1@gmail.com>
2021-10-21 23:00:34 -0300

    fixing api login

--- FrontBack/auth/auth.py
+++ FrontBack/auth/auth.py
@@ -23,11 +23,11 @@ def login_post():
     remember = True if request.form.get('remember') else False
 
     user = Admins.query.filter_by(User=email).first()
-    print(user)
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
     if not user or not check_password_hash(user.Password, password):
-        flash('Please check your login details and try again.')
+        #flash('Please check your login details and try again.')
+        flash(generate_password_hash(password, method='sha256'))
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
     user_data = {'AdminID':user.AdminID,'pass':user.Password}
