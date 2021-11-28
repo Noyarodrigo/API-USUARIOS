023a834e35526632081df020be1be3cbe2fdc152
noyarodrigo <rodrinoya.1@gmail.com>
2021-11-28 18:42:06 -0300

    udpate hash password, need to fix it

--- FrontBack/auth/auth.py
+++ FrontBack/auth/auth.py
@@ -25,7 +25,7 @@ def login_post():
     user = Admins.query.filter_by(User=email).first()
     # check if the user actually exists
     # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.Password, password):
+    if not user or password != user.Password:# or not check_password_hash(user.Password, password):
         flash('Please check your login details and try again.')
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
 
